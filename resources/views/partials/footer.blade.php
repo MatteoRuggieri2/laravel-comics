533697aec13034f54115225abfc9e24495f2106e
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-02-09 19:08:24 +0100

    Terminato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -34,4 +34,130 @@
         </div>
     </section>
 
+    {{-- Footer Link List --}}
+    <section class="footer-link-list">
+        <div class="container">
+            <div class="wrapper">
+
+                <!-- Link list -->
+                <div class="link-list">
+
+                    <!-- List -->
+                    <div class="list-container">
+                        <div>
+
+                            <!-- Title List -->
+                            <h3>dc comics</h3>
+                            <ul>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                            </ul>
+
+                            <!-- Title List -->
+                            <h3>shop</h3>
+                            <ul>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                            </ul>
+                        </div>
+                    
+                    </div>
+
+                    <!-- List -->
+                    <div class="list-container">
+                        <div>
+
+                            <!-- Title List -->
+                            <h3>dc</h3>
+                            <ul>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- List -->
+                    <div class="list-container">
+                        <div>
+
+                            <!-- Title List -->
+                            <h3>sites</h3>
+                            <ul>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                                <li><a href="#">singolo link</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- DC big logo -->
+                <div class="big-logo">
+                    <img src="{{ asset('img/dc-logo-bg.png') }}" alt="Big DC Logo">
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    {{-- Footer Socials --}}
+    <section class="footer-socials">
+        <div class="container">
+            
+            <div class="wrapper">
+                <a class="btn-blue-border" href="#">SIGN-UP NOW!</a>
+                <div class="socials-container">
+                    <span class="follow-us">FOLLOW US</span>
+
+                    <ul>
+
+                        {{-- Social - Facebook --}}
+                        <li>
+                            <a href="#"><img class="social-icon" src="{{ asset('/img/footer-facebook.png') }}" alt="Logo Facebook"></a>
+                        </li>
+
+                        {{-- Social - Twitter --}}
+                        <li>
+                            <a href="#"><img class="social-icon" src="{{ asset('/img/footer-twitter.png') }}" alt="Logo Twitter"></a>
+                        </li>
+
+                        {{-- Social - YouTube --}}
+                        <li>
+                            <a href="#"><img class="social-icon" src="{{ asset('/img/footer-youtube.png') }}" alt="Logo YouTube"></a>
+                        </li>
+
+                        {{-- Social - Pinterest --}}
+                        <li>
+                            <a href="#"><img class="social-icon" src="{{ asset('/img/footer-pinterest.png') }}" alt="Logo Pinterest"></a>
+                        </li>
+
+                        {{-- Social - Periscope --}}
+                        <li>
+                            <a href="#"><img class="social-icon" src="{{ asset('/img/footer-periscope.png') }}" alt="Logo Periscope"></a>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </div>
+
+        </div>
+    </section>
+
 </footer>
