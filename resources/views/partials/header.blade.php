6ce83c7c3a90b468069358604c11fd3598d08113
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-02-13 19:10:53 +0100

    Aggiunto classe active dinamica

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     {{-- Pre-Header --}}
     <div class="pre-header">
         <div class="container">
-            <span class="pre-header-info">DC POWERsmVISAr</span>
+            <span class="pre-header-info">DC POWER<span>&#8480;</span>VISA<span>&reg;</span></span>
             <span class="pre-header-info">ADDITIONAL DC SITES</span>
         </div>
     </div>
@@ -15,16 +15,16 @@
                 <a href="{{ route('home') }}"><img src="{{ asset('/img/dc-logo.png') }}" alt="DC Logo"></a>
             </div>
             <ul>
-                <li><a href="{{ route('characters') }}">characters</a></li>
-                <li class="active"><a href="#">comics</a></li>
-                <li><a href="{{ route('movies') }}">movies</a></li>
-                <li><a href="#">tv</a></li>
-                <li><a href="#">games</a></li>
-                <li><a href="#">collectibles</a></li>
-                <li><a href="#">videos</a></li>
-                <li><a href="#">fans</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">shop</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'characters' ? 'active' : '' }}"><a href="{{ route('characters') }}">characters</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'comics' ? 'active' : '' }}"><a href="#">comics</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'movies' ? 'active' : '' }}"><a href="{{ route('movies') }}">movies</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'tv' ? 'active' : '' }}"><a href="#">tv</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'games' ? 'active' : '' }}"><a href="#">games</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'collectibles' ? 'active' : '' }}"><a href="#">collectibles</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'videos' ? 'active' : '' }}"><a href="#">videos</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'fans' ? 'active' : '' }}"><a href="#">fans</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'news' ? 'active' : '' }}"><a href="#">news</a></li>
+                <li class="{{ Request::route() && Request::route()->getName() === 'shop' ? 'active' : '' }}"><a href="#">shop</a></li>
             </ul>
         </nav>
     </div>
